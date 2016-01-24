8c744682ae9f8c513c3703c0f991c71fcfb0b3b7
levi-rs <levi.noecker@rewardstyle.com>
2016-01-24 16:34:21 -0600

    Add channel and subreddit stuff to config

--- dankbot/cli.py
+++ dankbot/cli.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import os
 from configparser import ConfigParser
 
@@ -9,15 +11,17 @@ class DankBot(object):
     '''
     Bot for posting dank memes from reddit to slack
     '''
-    def __init__(self, slack_token):
+    def __init__(self, slack_token, channel, subreddits):
         self.slack_token = slack_token
+        self.channel = channel
+        self.subreddits = subreddits
 
     def go(self):
         # Check for most recent dank memes
         memes = self.get_memes()
 
         # Filter out any known dank memes
-        filtered_memes = [meme for meme in memes if not self.in_collection(meme)]
+        filtered_memes = [m for m in memes if not self.in_collection(m)]
 
         # If any are left, post to slack
         self.post_to_slack(filtered_memes)
@@ -28,12 +32,17 @@ class DankBot(object):
         '''
 
         # Build the user_agent, this is important to conform to Reddit's rules
-        user_agent = 'linux:dankscraper:0.0.1 (by /u/IHKAS1984)'
+        user_agent = 'linux:dankscraper:0.0.2 (by /u/IHKAS1984)'
 
         # Create connection object
         r = praw.Reddit(user_agent=user_agent)
 
-        return [h.url for h in r.get_subreddit('dankmemes').get_hot()]
+        memes = list()
+
+        for subreddit in self.subreddits:
+            memes += [h.url for h in r.get_subreddit(subreddit).get_hot()]
+
+        return memes
 
     def in_collection(self, meme):
         '''
@@ -54,8 +63,7 @@ class DankBot(object):
         '''
         slack = Slacker(self.slack_token)
         for meme in memes:
-            #print(meme)
-            resp = slack.chat.post_message('#random', meme, as_user=True)
+            resp = slack.chat.post_message(self.channel, meme, as_user=True)
             break
 
             '''
@@ -70,7 +78,11 @@ def main():
     config_path = os.path.join(os.path.dirname(__file__), u'dankbot.ini')
     config.read(config_path)
 
-    DankBot(slack_token=config['slack']['token']).go()
+    token = config['slack']['token']
+    channel = config['slack']['channel']
+    subreddits = [s.strip(',') for s in config['reddit']['subreddits'].split()]
+
+    DankBot(slack_token=token, channel=channel, subreddits=subreddits).go()
 
 
 if __name__ == "__main__":
