003c75e997f8a6b6c88c1289f1c18398d4e9ff23
levi-rs <levi.noecker@rewardstyle.com>
2016-01-24 19:58:55 -0600

    Add mysql connection and queries

--- dankbot/cli.py
+++ dankbot/cli.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
 
 import os
+from datetime import datetime as dt
 from configparser import ConfigParser
 
 import praw
+import MySQLdb as mdb
 from slacker import Slacker
 
 
@@ -11,10 +13,12 @@ class DankBot(object):
     '''
     Bot for posting dank memes from reddit to slack
     '''
-    def __init__(self, slack_token, channel, subreddits):
+    def __init__(self, slack_token, channel, subreddits, username, password):
         self.slack_token = slack_token
         self.channel = channel
         self.subreddits = subreddits
+        self.username = username
+        self.password = password
 
     def go(self):
         # Check for most recent dank memes
@@ -39,8 +43,10 @@ class DankBot(object):
 
         memes = list()
 
-        for subreddit in self.subreddits:
-            memes += [h.url for h in r.get_subreddit(subreddit).get_hot()]
+        func = r.get_subreddit
+
+        for sub in self.subreddits:
+            memes += [Meme(link.url, sub) for link in func(sub).get_hot()]
 
         return memes
 
@@ -49,13 +55,33 @@ class DankBot(object):
         Checks to see if the supplied meme is already in the collection of known
         memes
         '''
-        pass
+        query = "select * from memes where links = '{0}'".format(meme.link)
+
+        con = mdb.connect(
+                'localhost', self.username, self.password, 'danklinks')
+
+        with con:
+            cur = con.cursor()
+            resp = cur.execute(query)
+
+        return True if resp else False
 
     def add_to_collection(self, meme):
         '''
         Adds a meme to the collection
         '''
-        pass
+        query = """INSERT INTO memes 
+                   (links, sources, datecreated) 
+                   VALUES
+                   ('{0}', '{1}', '{2}')
+                """.format(meme.link, meme.source, str(dt.now()))
+
+        con = mdb.connect(
+                'localhost', self.username, self.password, 'danklinks')
+
+        with con:
+            cur = con.cursor()
+            cur.execute(query)
 
     def post_to_slack(self, memes):
         '''
@@ -63,13 +89,19 @@ class DankBot(object):
         '''
         slack = Slacker(self.slack_token)
         for meme in memes:
-            resp = slack.chat.post_message(self.channel, meme, as_user=True)
-            break
+            resp = slack.chat.post_message(self.channel, meme.link, as_user=True)
 
-            '''
             if resp.successful:
                 self.add_to_collection(meme)
-            '''
+
+
+class Meme(object):
+    """
+    Model for meme objects
+    """
+    def __init__(self, link, source):
+        self.link = link
+        self.source = source
 
 
 def main():
@@ -80,9 +112,18 @@ def main():
 
     token = config['slack']['token']
     channel = config['slack']['channel']
+    username = config['mysql']['username']
+    password = config['mysql']['password']
+
     subreddits = [s.strip(',') for s in config['reddit']['subreddits'].split()]
 
-    DankBot(slack_token=token, channel=channel, subreddits=subreddits).go()
+    DankBot(
+        slack_token=token,
+        channel=channel,
+        subreddits=subreddits,
+        username=username,
+        password=password
+    ).go()
 
 
 if __name__ == "__main__":
