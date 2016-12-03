122c655a7708da53e514d1f9d5ad5e780416843b
levi-rs <levi.noecker@rewardstyle.com>
2016-12-03 08:07:20 -0600

    Mock out logging

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,19 +1,21 @@
-from logging import Logger
 from configparser import ConfigParser
 from unittest.mock import patch
 
 from dankbot import cli
 
 
+@patch('dankbot.cli.logging.getLogger')
+@patch('dankbot.cli.RotatingFileHandler')
 @patch('dankbot.cli.DankBot')
-def test_main(dankbot):
+def test_main(dankbot, _, gl_mock):
+    gl_mock.return_value = gl_mock
     dankbot.return_value = dankbot
 
     cli.main()
 
     assert dankbot.called
     assert isinstance(dankbot.call_args[0][0], ConfigParser)
-    assert isinstance(dankbot.call_args[0][1], Logger)
+    assert dankbot.call_args[0][1], gl_mock
     assert dankbot.find_and_post_memes.called
 
 
