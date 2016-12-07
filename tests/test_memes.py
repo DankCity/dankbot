2a9f2784d59c4713bd97222fece62109bde53797
levi-rs <levi.noecker@rewardstyle.com>
2016-12-07 16:24:23 -0600

    Make testing more DRY
     - Move fixtures to conftest
     - Move string definitions to __init__

--- tests/test_memes.py
+++ tests/test_memes.py
@@ -1,130 +1,40 @@
 import pytest
-from unittest.mock import patch
 
-from dankbot.memes import ImgurMeme, DankMeme, Meme, UndigestedError
-
-TEST_LINK = "test meme link"
-TEST_SUBREDDIT = "test subreddit"
-TEST_SUBREDDIT_2 = "test subreddit 2"
-TEST_SUBREDDIT_3 = "test subreddit 3"
-TEST_SUBREDDIT_4 = "test subreddit 4"
-TEST_REDDIT_ID = "test reddit id"
-
-CLIENT_ID = "mock_id"
-CLIENT_SECRET = "mock_secret"
-
-TEST_DIRECT_LINK = "http://i.imgur.com/abcdef.pdf"
-
-TEST_IMAGE_LINK = "http://imgur.com/abcdef"
-
-ALBUM_1 = 'album id 1'
-TEST_ALBUM_LINK_1 = "http://imgur.com/a/{0}".format(ALBUM_1)
-
-ALBUM_2 = 'album id 2'
-TEST_ALBUM_LINK_2 = "http://imgur.com/a/{0}".format(ALBUM_2)
-
-GALLERY_1 = 'gallery id 1'
-TEST_GALLERY_LINK_1 = "http://imgur.com/g/{0}".format(GALLERY_1)
-
-IMAGE_1 = 'image id 1'
-TEST_IMAGE_LINK_1 = "http://imgur.com/{0}".format(IMAGE_1)
-
-
-class RedditMeme(object):
-    def __init__(self, sub=TEST_SUBREDDIT, url=TEST_LINK, id_=TEST_REDDIT_ID):
-            self.subreddit = sub
-            self.url = url
-            self.id = id_
-
-
-@pytest.fixture
-def meme():
-    return Meme(RedditMeme())
-
-
-@pytest.fixture
-def dank_meme():
-    return DankMeme(RedditMeme())
-
-
-@pytest.fixture
-def imgur_direct_meme():
-    ImgurMeme.set_credentials(CLIENT_ID, CLIENT_SECRET)
-    return ImgurMeme(RedditMeme(url=TEST_DIRECT_LINK))
-
-
-@pytest.fixture
-def imgur_image_meme():
-    ImgurMeme.set_credentials(CLIENT_ID, CLIENT_SECRET)
-    return ImgurMeme(RedditMeme(url=TEST_IMAGE_LINK))
-
-
-@pytest.fixture
-def imgur_image_meme_2():
-    ImgurMeme.set_credentials(CLIENT_ID, CLIENT_SECRET)
-    return ImgurMeme(RedditMeme(sub=TEST_SUBREDDIT_4, url=TEST_IMAGE_LINK_1))
-
-
-@pytest.fixture
-def imgur_album_meme_1():
-    ImgurMeme.set_credentials(CLIENT_ID, CLIENT_SECRET)
-    return ImgurMeme(RedditMeme(url=TEST_ALBUM_LINK_1))
-
-
-@pytest.fixture
-def imgur_album_meme_2():
-    ImgurMeme.set_credentials(CLIENT_ID, CLIENT_SECRET)
-    return ImgurMeme(RedditMeme(sub=TEST_SUBREDDIT_2, url=TEST_ALBUM_LINK_2))
-
-
-@pytest.fixture
-def imgur_gallery_meme_1():
-    ImgurMeme.set_credentials(CLIENT_ID, CLIENT_SECRET)
-    return ImgurMeme(RedditMeme(sub=TEST_SUBREDDIT_3, url=TEST_GALLERY_LINK_1))
-
-
-@pytest.fixture
-def client():
-    with patch("dankbot.memes.ImgurClient") as client:
-        client.return_value = client
-        client.get_image.return_value = client
-        client.gallery_item.return_value = client
-        client.get_album.return_value = client
-
-        yield client
+from tests import RedditMeme, test_vars as tv
+from dankbot.memes import ImgurMeme, UndigestedError
 
 
 def test_meme(meme):
     """ Tests the base Meme class
     """
-    assert meme.link == TEST_LINK
-    assert meme.source == TEST_SUBREDDIT
-    assert str(meme) == TEST_LINK
-    assert repr(meme) == "from {0}: {1}".format(TEST_SUBREDDIT, TEST_LINK)
+    assert meme.link == tv['TEST_LINK']
+    assert meme.source == tv['TEST_SUBREDDIT']
+    assert str(meme) == tv['TEST_LINK']
+    assert repr(meme) == "from {0}: {1}".format(tv['TEST_SUBREDDIT'], tv['TEST_LINK'])
     assert meme.format_for_slack() == repr(meme)
 
 
 def test_dankmeme(dank_meme):
     """ Tests the DankMeme subclass
     """
-    assert dank_meme.link == TEST_LINK
-    assert dank_meme.source == TEST_SUBREDDIT
-    assert str(dank_meme) == TEST_LINK
-    assert repr(dank_meme) == "from {0}: {1}".format(TEST_SUBREDDIT, TEST_LINK)
+    assert dank_meme.link == tv['TEST_LINK']
+    assert dank_meme.source == tv['TEST_SUBREDDIT']
+    assert str(dank_meme) == tv['TEST_LINK']
+    assert repr(dank_meme) == "from {0}: {1}".format(tv['TEST_SUBREDDIT'], tv['TEST_LINK'])
     assert dank_meme.format_for_slack() == repr(dank_meme)
 
 
 def test_ImgurMeme_credential_setting():
     """ Tests the ImgurMeme subclass credential setting
     """
-    reddit_meme = RedditMeme(url=TEST_IMAGE_LINK)
+    reddit_meme = RedditMeme(url=tv['TEST_IMAGE_LINK'])
 
-    ImgurMeme.set_credentials(CLIENT_ID, CLIENT_SECRET)
+    ImgurMeme.set_credentials(tv['CLIENT_ID'], tv['CLIENT_SECRET'])
 
     i_meme = ImgurMeme(reddit_meme)
 
-    assert i_meme.client_id == CLIENT_ID
-    assert i_meme.client_secret == CLIENT_SECRET
+    assert i_meme.client_id == tv['CLIENT_ID']
+    assert i_meme.client_secret == tv['CLIENT_SECRET']
 
 
 def test_ImgurMeme_get_client_exception(client):
@@ -133,7 +43,7 @@ def test_ImgurMeme_get_client_exception(client):
     # Setup the ImgurMeme object
     ImgurMeme.set_credentials(None, None)
 
-    i_meme = ImgurMeme(RedditMeme(url=TEST_IMAGE_LINK))
+    i_meme = ImgurMeme(RedditMeme(url=tv['TEST_IMAGE_LINK']))
 
     with pytest.raises(ValueError) as excstr:
         i_meme.digest()
@@ -147,9 +57,9 @@ def test_ImgurMeme_get_client(client):
     """ Test the imgur client creation functionality
     """
     # Setup the ImgurMeme object
-    ImgurMeme.set_credentials(CLIENT_ID, CLIENT_SECRET)
+    ImgurMeme.set_credentials(tv['CLIENT_ID'], tv['CLIENT_SECRET'])
 
-    ImgurMeme(RedditMeme(url=TEST_IMAGE_LINK)).digest()
+    ImgurMeme(RedditMeme(url=tv['TEST_IMAGE_LINK'])).digest()
 
     assert client.called
 
@@ -169,8 +79,8 @@ def test_ImgurMeme_direct_link(client, imgur_direct_meme):
     assert imgur_direct_meme.link_type is ImgurMeme.DIRECT_LINK
     assert imgur_direct_meme.image_count is None
     assert imgur_direct_meme.first_image_link is None
-    assert TEST_SUBREDDIT in slack_str
-    assert TEST_DIRECT_LINK in slack_str
+    assert tv['TEST_SUBREDDIT'] in slack_str
+    assert tv['TEST_DIRECT_LINK'] in slack_str
     assert "more at" not in slack_str
 
 
@@ -192,11 +102,11 @@ def test_ImgurMeme_album_01(client, imgur_album_meme_1):
     assert not client.get_image.called
     assert not client.gallery_item.called
     assert imgur_album_meme_1.link_type is ImgurMeme.ALBUM_LINK
-    client.get_album.assert_called_with(ALBUM_1)
+    client.get_album.assert_called_with(tv['ALBUM_1'])
     assert imgur_album_meme_1.image_count == image_count
     assert imgur_album_meme_1.first_image_link == fake_link
-    assert TEST_SUBREDDIT in slack_str
-    assert TEST_ALBUM_LINK_1 in slack_str
+    assert tv['TEST_SUBREDDIT'] in slack_str
+    assert tv['TEST_ALBUM_LINK_1'] in slack_str
     assert fake_link in slack_str
     assert "more at" in slack_str
 
@@ -214,7 +124,7 @@ def test_ImgurMeme_album_02(client, imgur_album_meme_2):
     assert not client.get_image.called
     assert not client.gallery_item.called
     assert imgur_album_meme_2.link_type is ImgurMeme.ALBUM_LINK
-    client.get_album.assert_called_with(ALBUM_2)
+    client.get_album.assert_called_with(tv['ALBUM_2'])
     assert imgur_album_meme_2.image_count == image_count
     assert imgur_album_meme_2.first_image_link == fake_link
 
@@ -237,11 +147,11 @@ def test_ImgurMeme_gallery_01(client, imgur_gallery_meme_1):
     assert not client.get_image.called
     assert not client.album_item.called
     assert imgur_gallery_meme_1.link_type is ImgurMeme.GALLERY_LINK
-    client.gallery_item.assert_called_with(GALLERY_1)
+    client.gallery_item.assert_called_with(tv['GALLERY_1'])
     assert imgur_gallery_meme_1.image_count == image_count
     assert imgur_gallery_meme_1.first_image_link == fake_link
-    assert TEST_SUBREDDIT_3 in slack_str
-    assert TEST_GALLERY_LINK_1 in slack_str
+    assert tv['TEST_SUBREDDIT_3'] in slack_str
+    assert tv['TEST_GALLERY_LINK_1'] in slack_str
     assert fake_link in slack_str
     assert "more at" in slack_str
 
@@ -264,7 +174,7 @@ def test_ImgurMeme_gallery_02(client, imgur_gallery_meme_1):
     assert not client.get_image.called
     assert not client.album_item.called
     assert imgur_gallery_meme_1.link_type is ImgurMeme.GALLERY_LINK
-    client.gallery_item.assert_called_with(GALLERY_1)
+    client.gallery_item.assert_called_with(tv['GALLERY_1'])
     assert imgur_gallery_meme_1.image_count == 0
     assert imgur_gallery_meme_1.first_image_link == other_fake_link
 
@@ -285,10 +195,10 @@ def test_ImgurMeme_image_01(client, imgur_image_meme_2):
     assert not client.album_item.called
     assert not client.gallery_item.called
     assert imgur_image_meme_2.link_type is ImgurMeme.IMAGE_LINK
-    client.get_image.assert_called_with(IMAGE_1)
+    client.get_image.assert_called_with(tv['IMAGE_1'])
     assert imgur_image_meme_2.image_count == 0
     assert imgur_image_meme_2.first_image_link == other_fake_link
-    assert TEST_SUBREDDIT_4 in slack_str
+    assert tv['TEST_SUBREDDIT_4'] in slack_str
     assert other_fake_link in slack_str
     assert "more at" not in slack_str
 
